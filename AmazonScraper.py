2c3cd8daa83a6d0a9bababecdbe8191f6794339a
ColleSerre <61471511+ColleSerre@users.noreply.github.com>
2020-05-06 12:08:43 +0200

    Add files via upload

--- AmazonScraper.py
+++ AmazonScraper.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup as BS
 from urllib.request import urlopen
-import sys
 
 class page:
     def __init__(self, url):
@@ -8,32 +7,41 @@ class page:
         self.bs = BS(self.url, 'html.parser')
     def getprice(self):
         raw_price = self.bs.find('span', {'class':'a-size-medium a-color-price offer-price a-text-normal'}).get_text()
-        # add removal of non-digits using regexs
         for char in raw_price:
             if char.isnumeric() == False:
                 raw_price.replace(char, '')
-        print(raw_price)
-
+        print('Price: ' + raw_price)
+    def getname(self):
+        raw_name = self.bs.find('span', {'id' : 'productTitle'}).get_text()
+        print('Product name: ' + raw_name.strip())
+    
+url = input('Copy-past an amazon url: ')
+Page = page(url)
 welcome=print("""
-----------------------------------------------------------
-|                                                        |
-|                     Web Scraping API                   |
-|                                                        |
-|   Features:                                            |
-|                                                        |
-|      * automate prices  getters from amazon web pages  |
-|        {1}--get Price from Amazon pages                |
-|                                                        |
-|   Features to come:                                    |
-|                                                        |
-|       * Enter a .txt file and get prices for each line |
-|                                                        |
-----------------------------------------------------------""")
-prompt = int(input())
+                ----------------------------------------------------------
+                |                                                        |
+                |                   Web Scraping API                     |
+                |                                                        |
+                |   Features:                                            |
+                |                                                        |
+                |      {1}--get price from Amazon pages                  |
+                |      {2}--get product name from Amazon pages           |
+                |                                                        |
+                |   Features to come:                                    |
+                |                                                        |
+                |       * Enter a .txt file and get prices for each line |
+                |                                                        |
+                ----------------------------------------------------------""")
+
+prompt = 0
+try:
+    prompt = int(input('##'))
+except:
+    print('Invalid Input')
+    while prompt == 0:
+        prompt = int(input('##'))
 
 if prompt == 1:
-    url = input('Copy-paste an amazon url ')
-    Page = page(url)
     Page.getprice()
-else:
-    print('Invalid input')
+elif prompt == 2:
+    Page.getname()
