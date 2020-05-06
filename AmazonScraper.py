042f4c14a5dae4e725ebbe4ec6f70cb502323b29
ColleSerre <61471511+ColleSerre@users.noreply.github.com>
2020-05-06 14:52:54 +0200

    Adding "get vendor" feature & more
    Adding "get vendor" feature ; try/except syntax for the other methods

--- AmazonScraper.py
+++ AmazonScraper.py
@@ -1,23 +1,33 @@
 from bs4 import BeautifulSoup as BS
 from urllib.request import urlopen
+import time
 
 class page:
     def __init__(self, url):
         self.url = urlopen(url)
         self.bs = BS(self.url, 'html.parser')
     def getprice(self):
-        raw_price = self.bs.find('span', {'class':'a-size-medium a-color-price offer-price a-text-normal'}).get_text()
-        for char in raw_price:
-            if char.isnumeric() == False:
-                raw_price.replace(char, '')
-        print('Price: ' + raw_price)
+        try:
+            raw_price = self.bs.find('span', {'id':'price_inside_buybox'}).get_text()
+        except:
+            raw_price = self.bs.find('span', {'class':'a-size-medium a-color-price offer-price a-text-normal'}).get_text()
+        print('Price: ' + raw_price.strip())
     def getname(self):
-        raw_name = self.bs.find('span', {'id' : 'productTitle'}).get_text()
+        try:
+            raw_name = self.bs.find('span', {'id' : 'productTitle'}).get_text()
+        except:
+            raw_name = self.bs.find('span', {'class' : 'a-size-extra-large'}).get_text()
         print('Product name: ' + raw_name.strip())
+    def getvendor(self):
+        try:
+            raw_vendor = self.bs.find('a', {'id':'bylineInfo'}).get_text()
+        except:
+            raw_vendor = self.bs.find('a', {'class':'a-link-normal'}).get_text()
+        print('Vendor: ' + raw_vendor)
     
-url = input('Copy-past an amazon url: ')
+url = input('Paste an Amazon URL: ')
 Page = page(url)
-welcome=print("""
+welcome="""
                 ----------------------------------------------------------
                 |                                                        |
                 |                   Web Scraping API                     |
@@ -26,22 +36,35 @@ welcome=print("""
                 |                                                        |
                 |      {1}--get price from Amazon pages                  |
                 |      {2}--get product name from Amazon pages           |
+                |      {3}--get vendor name from Amazon pages            |
                 |                                                        |
                 |   Features to come:                                    |
                 |                                                        |
                 |       * Enter a .txt file and get prices for each line |
                 |                                                        |
-                ----------------------------------------------------------""")
+                ----------------------------------------------------------"""
 
-prompt = 0
-try:
-    prompt = int(input('##'))
-except:
-    print('Invalid Input')
-    while prompt == 0:
+def actions():
+    print(welcome)
+    try:
         prompt = int(input('##'))
+    except:
+        print('Invalid Input')
+        prompt = int(input('##'))
+        time.sleep(1)
+        actions()
+    if prompt == 1:
+        Page.getprice()
+        time.sleep(1)
+        actions()
+    elif prompt == 2:
+        Page.getname()
+        time.sleep(1)
+        actions()
+    elif prompt == 3:
+        Page.getvendor()
+        time.sleep(1)
+        actions()
 
-if prompt == 1:
-    Page.getprice()
-elif prompt == 2:
-    Page.getname()
+if __name__ == "__main__":
+    actions()
